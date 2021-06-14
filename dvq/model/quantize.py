Fix references, more efficient perplexity computation

dvq/model/quantize.py
@@ -44,7 +44,7 @@ class VQVAEQuantize(nn.Module):
         self.r_reestim = 5000
         self.reservoir = []
         self.max_cache_size = 1000000 // (200 * 128 // 10) # N - max items
-        self.kmeans = faiss.Kmeans(embedding_dim, n_embed, gpu=True)
+        self.kmeans = faiss.Kmeans(self.embedding_dim, self.n_embed, gpu=True)
 
     def forward(self, z):
         B, C, H, W = z.size()

dvq/test.py
@@ -48,8 +48,8 @@ for img, label in data_module.val_dataloader():
     l = torch.nn.functional.mse_loss(x_hat, img)
     mse.append(l.item())
 
-codes = torch.cat(codes)
+codes = torch.cat(codes).flatten()
 with torch.no_grad():
     print(codes.shape)
     perplexity, code_use = vq.compute_metrics(codes)
-print('P:', perplexity.item(), 'C:', code_use.item(), 'M:', torch.mean(torch.as_tensor(mse)).item())
+print('P:', perplexity.item(), 'C:', code_use, 'M:', torch.mean(torch.as_tensor(mse)).item())

dvq/vqvae.py
@@ -65,10 +65,10 @@ class VQVAE(pl.LightningModule):
         return x_hat, latent_loss, ind
 
     def compute_metrics(self, ind):
-        encodings = F.one_hot(ind, self.quantizer.n_embed).float().reshape(-1, self.quantizer.n_embed)
-        avg_probs = encodings.mean(0)
+        counts = ind.unique(return_counts=True)[1]
+        avg_probs = counts / len(ind)
         perplexity = (-(avg_probs * torch.log(avg_probs + 1e-10)).sum()).exp()
-        cluster_use = torch.sum(avg_probs > 0)
+        cluster_use = len(counts)
         return perplexity, cluster_use
 
     def training_step(self, batch, batch_idx):